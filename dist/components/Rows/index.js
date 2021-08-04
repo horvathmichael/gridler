6ceb5f4f5fdc14b065abde36aeed0db35c02b2ce
horvathmichael <office@horvathmichael.at>
2021-08-04 20:08:47 +0200

    build

---
+++ dist/components/Rows/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _Rows = _interopRequireDefault(require("./Rows"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _default = _Rows.default;
+exports.default = _default;
